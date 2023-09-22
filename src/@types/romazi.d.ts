b6d6f7f6b6fb634d311b7d929e255b550a5043c7
ojako <youthfulday8348@gmail.com>
2023-09-23 06:47:05 +0900

    goo-apiを利用した漢字変換

--- src/@types/romazi.d.ts
+++ src/@types/romazi.d.ts
@@ -1,3 +1,7 @@
 declare module "js-hira-kata-romanize";
-declare module 'kuroshiro';
-declare module "kuroshiro-analyzer-kuromoji";
+// declare module 'kuroshiro';
+// declare module "kuroshiro-analyzer-kuromoji";
+// declare module "path-browserify" {
+//   import path from "path";
+//   export default path;
+// }
--- src/Components/saveTypingData.ts
+++ src/Components/saveTypingData.ts
@@ -1,49 +1,49 @@
 import axios from "axios";
 import { type InputType } from "../@types/PagesTypes";
 import Romanizer from "js-hira-kata-romanize";
-import Kuroshiro from "kuroshiro";
-import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji";
+// import Kuroshiro from "kuroshiro";
+// import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji";
 
 const saveTypingData = (typingdata: InputType): void => {
-  const kuroshiro = new Kuroshiro();
-  kuroshiro
-    .init(new KuromojiAnalyzer())
-    .then(function () {
-      return kuroshiro.convert(
-        "感じ取れたら手を繋ごう、重なるのは人生のライン and レミリア最高！",
-        { to: "hiragana" }
-      );
-    })
-    .then(function (result: any) {
-      console.log(result);
-    });
-  // const romanizer = new Romanizer({
-  //   mapping: Romanizer.MAPPING_KUNREI,
-  //   chouon: Romanizer.CHOUON_SKIP,
-  //   upper: Romanizer.UPPER_ALL,
-  // });
+  // const kuroshiro = new Kuroshiro();
+  // kuroshiro
+  //   .init(new KuromojiAnalyzer())
+  //   .then(function () {
+  //     return kuroshiro.convert(
+  //       "感じ取れたら手を繋ごう、重なるのは人生のライン and レミリア最高！",
+  //       { to: "hiragana" }
+  //     );
+  //   })
+  //   .then(function (result: any) {
+  //     console.log(result);　半角数字テスト1234567890
+  //   });　この品物は$400ですが、本当は￥いくらですか？また「バナナ＆いちご１００％！」かしら
+  const romanizer = new Romanizer({
+    mapping: Romanizer.MAPPING_KUNREI,
+    chouon: Romanizer.CHOUON_SKIP,
+    upper: Romanizer.UPPER_ALL,
+  });
   let apiUrl: string = "";
   let word: string = "";
-  // typingdata.problems.map((problem) => {
-  //   //gooひらがなAPIに必要な情報を記載し、HTTP POSTするための設定
-  //   apiUrl = "https://labs.goo.ne.jp/api/hiragana";
-  //   //gooひらがな化APIで変換したいテキスト
-  //   word = problem.text;
-  //   let payload = {
-  //     app_id:
-  //       "932681ca25180b398a951d1e63f4791bc7575ff9e510af74e96f5a9ea0bc5938",
-  //     sentence: word,
-  //     output_type: "hiragana",
-  //   };
-  //   axios
-  //     .post(apiUrl, payload)
-  //     .then((results) => {
-  //       problem.kana = results.data["converted"];          // かな変換
-  //       problem.romazi = romanizer.romanize(problem.kana); // ローマ字変換
-  //     })
-  //     .catch((error) => console.log("失敗したようです。", error.status));
-  // });
-  // console.log(typingdata.problems);
+  typingdata.problems.map((problem) => {
+    //gooひらがなAPIに必要な情報を記載し、HTTP POSTするための設定
+    apiUrl = "https://labs.goo.ne.jp/api/hiragana";
+    //gooひらがな化APIで変換したいテキスト
+    word = problem.text;
+    let payload = {
+      app_id:
+        "932681ca25180b398a951d1e63f4791bc7575ff9e510af74e96f5a9ea0bc5938",
+      sentence: word,
+      output_type: "hiragana",
+    };
+    axios
+      .post(apiUrl, payload)
+      .then((results) => {
+        problem.kana = results.data["converted"]; // かな変換
+        problem.romazi = romanizer.romanize(problem.kana); // ローマ字変換
+      })
+      .catch((error) => console.log("失敗したようです。", error.status));
+  });
+  console.log(typingdata.problems);
 
   // 登録されたデータが有れば取得
   // if (localStorage.hasOwnProperty("typingData")) {
