ff8b27c5fd50ecfe7633550d6b79b6cccf3cd158
ojako <youthfulday8348@gmail.com>
2023-12-11 21:19:07 +0900

    articleの件数表示ロジック実装

--- src/Components/HomeProblemList/TypingLists.tsx
+++ src/Components/HomeProblemList/TypingLists.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { Grid } from "@mui/material";
 import { TypingDataType } from "../../@types";
 import { TypingDataContext } from "../../Contexts/TypingDataContext";
@@ -7,27 +7,34 @@ import TypingItem from "./TypingItem";
 import { randomArray } from "../../Hooks";
 
 export default function TypingLists() {
-  const { typingdatas, setTypingData } = React.useContext(TypingDataContext);
+  const { typingdatas, setTypingDatas, setTypingData } =
+    React.useContext(TypingDataContext);
   const scrollTop = useScrollToTop();
-  const selectedTypingToTop = (post: TypingDataType): void => {
-    scrollTop();
-    setTypingData(post);
-  };
+
+  const selectedTypingToTop = useCallback(
+    (post: TypingDataType): void => {
+      scrollTop();
+      setTypingData(post);
+    },
+    [scrollTop, setTypingData]
+  );
 
   // 最大表示件数を制限
-  const datas: TypingDataType[] = [];
   useEffect(() => {
-    let rand = randomArray(typingdatas.length, 9)
+    const datas: TypingDataType[] = [];
+    // Articleは一旦ランダム表示として実装
+    let rand = randomArray(typingdatas.length, 9);
     for (let i = 0; i < rand.length; i++) {
       datas.push(typingdatas[i]);
     }
-  }, [typingdatas]);
+    setTypingDatas(datas);
+  }, []);
 
   return (
     <Grid container spacing={{ md: 3 }} columns={{ md: 12 }}>
       {typingdatas &&
-        datas.map((post, index) => (
-          <Grid item xs={2} sm={4} md={4} key={post.id}>
+        typingdatas.map((post, index) => (
+          <Grid item xs={2} sm={4} md={4} key={`typingdata${index}`}>
             <TypingItem post={post} selectedTypingToTop={selectedTypingToTop} />
           </Grid>
         ))}
--- src/Hooks/randomArray.ts
+++ src/Hooks/randomArray.ts
@@ -8,11 +8,11 @@ export default function randomArray(dataLength: number, limit: number = dataLeng
   let randomAry = [];
   let a = [...Array(dataLength).keys()];
   while (a.length > 0) {
-    if(limit < count) continue;
     idx = Math.floor(Math.random() * a.length);
     randomAry.push(a[idx]);
     a.splice(idx, 1);
     count++;
+    if(limit <= count) a.length = 0;
   }
   return randomAry;
 }
