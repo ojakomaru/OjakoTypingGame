12e146e803357077806626fae0cc1cc00c37159f
ojako <youthfulday8348@gmail.com>
2024-01-24 06:07:29 +0900

    タイピングデータをfirebaseに登録。データの整形まで完了。

--- src/Contexts/TypingDataContext.tsx
+++ src/Contexts/TypingDataContext.tsx
@@ -1,8 +1,9 @@
-import React, { ReactNode, createContext, useState } from "react";
+import React, { ReactNode, createContext, useState, useEffect } from "react";
 import { useEffectOnce } from "../Hooks";
 import { TypingDataType, TypingGameData } from "../@types";
+import { useCollection } from "react-firebase-hooks/firestore";
 import { db } from "../Config";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc, collection, getDocs } from "firebase/firestore";
 
 let initialData: TypingDataType = {
   id: "1",
@@ -36,36 +37,25 @@ export const TypingDataProvider: React.FC<{ children: ReactNode }> = ({
   const [typingdatas, setTypingDatas] = useState<TypingDataType[]>(
     JSON.parse(localStorage.getItem("typingData") as string)
   );
+  const [typing_datas, setTyping_Datas] = useState<TypingDataType[]>([]);
+  const getAllTypingDatas = async () => {
+    const typingCollection = collection(db, "typingdatas");
+    const querySnapshot = await getDocs(typingCollection);
+    const tempJson = querySnapshot.docs.map((doc) => ({ ...doc.data() }));
+    let tempAry: TypingDataType[] = [];
+    for (let i = 0; i < tempJson.length; i++) {
+       tempAry.push(JSON.parse(tempJson[i].typingdata));
+    }
+    setTyping_Datas(tempAry);
+    // console.log(querySnapshot.docs.map((doc) => ({ ...doc.data() })));
+  };
+
   // メイン画面にタイピングデータを渡す
   useEffectOnce(() => {
-    const obj = JSON.stringify({
-      id: "1",
-      title: "test",
-      problems: [
-        {
-          text: "テスト問題",
-          kana: "てすともんだい",
-          romaji: "tesutomondai",
-          typingWords: [
-            ["te"],
-            ["su"],
-            ["to"],
-            ["mo"],
-            ["n", "xn", "nn"],
-            ["da"],
-            ["i", "yi"],
-          ],
-        },
-        {
-          text: "田中健太",
-          romaji: "tanakakenta",
-          kana: "タナカケンタ",
-          typingword: [["ta"], ["na"], ["ka", "ca"]],
-        },
-      ],
-    });
-    const cityRef = doc(db, "typingdatas", "ojakoAddDataTest");
-    setDoc(cityRef, { typingdata: obj });
+    getAllTypingDatas();
+    // const obj = JSON.stringify(typingdatas[8]);
+    // const cityRef = doc(db, "typingdatas", typingdatas[8].id);
+    // setDoc(cityRef, { typingdata: obj });
     if (typingdatas) {
       const rnd = Math.floor(Math.random() * typingdatas.length);
       setTypingData(typingdatas[rnd]);
